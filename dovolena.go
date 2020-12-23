bd5c217799f2d7ae25ff8f0f5d2301380ed4f3cc
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-12-23 20:58:58 +0100

    comments

--- dovolena.go
+++ dovolena.go
@@ -10,15 +10,33 @@ type peoples struct {
 	zenyI []map[int]int
 }
 
-func pair(group [][]int) (pairs []int) {
-	var block []bool
+type block struct {
+	block bool
+	who   int
+	where int
+}
 
-	block = make([]bool, len(group))
+func pair(group [][]int) (pairs []int) { // jdenostraně najde páry
+	var whoBlock []block
+
+	whoBlock = make([]block, len(group))
 	pairs = make([]int, len(group))
-	for i := 0; i < len(group); i++ {
-		for j := 0; j < len(group); j++ {
-			if block[group[i][j]] == false {
-				block[group[i][j]] = true
+	for i := 0; i < len(group); i++ { // projde všechny subjekty na spárování
+		for j := 0; j < len(group); j++ { // projde odpovídající opačná pohlaví
+			if whoBlock[group[i][j]].block == true {
+				if j < whoBlock[group[i][j]].where { // když už si někdo někoho zabral, ale já ho mám dřív
+					tmp := whoBlock[group[i][j]]
+					whoBlock[group[i][j]].who = i
+					whoBlock[group[i][j]].where = j
+					pairs[i] = group[i][j]
+					i = tmp.who
+					j = tmp.where
+					continue // prohodím a tomu komu jsem sebral protějšek najdu jiný
+				}
+			} else { // najdu někoho volného
+				whoBlock[group[i][j]].block = true
+				whoBlock[group[i][j]].who = i
+				whoBlock[group[i][j]].where = j
 				pairs[i] = group[i][j]
 				break
 			}
@@ -27,14 +45,14 @@ func pair(group [][]int) (pairs []int) {
 	return
 }
 
-func makePairs(relations *peoples, mWants []int, zWants []int) (pairs [][2]int) {
+func makePairs(relations *peoples, mWants []int, zWants []int) (pairs [][2]int) { // zkompletuje páry od obou stran
 	pairs = make([][2]int, 0)
 	for i, z := range mWants {
-		if zWants[z] == i {
+		if zWants[z] == i { // oba se chtějí, není co řešit
 			pairs = append(pairs, [2]int{i, z})
-		} else if zWants[z] < i {
+		} else if zWants[z] < i { // žena chce někoho už spárovaného, ten je spokojený, smůla
 			pairs = append(pairs, [2]int{i, z})
-		} else if relations.muziI[zWants[z]][z] < relations.muziI[zWants[z]][mWants[zWants[z]]] {
+		} else if relations.muziI[zWants[z]][z] < relations.muziI[zWants[z]][mWants[zWants[z]]] { // žena chce někoho ještě volného více, prohodím
 			mWants[i] = mWants[zWants[z]]
 			mWants[zWants[z]] = z
 			pairs = append(pairs, [2]int{i, mWants[i]})
@@ -54,7 +72,7 @@ func main() {
 	relations.zeny = make([][]int, N)
 	relations.muziI = make([]map[int]int, N)
 	relations.zenyI = make([]map[int]int, N)
-	for i := 0; i < N; i++ {
+	for i := 0; i < N; i++ { // načti data
 		relations.muzi[i] = make([]int, N)
 		relations.muziI[i] = make(map[int]int)
 		for j := 0; j < N; j++ {
@@ -72,9 +90,9 @@ func main() {
 			relations.zenyI[i][relations.zeny[i][j]] = j
 		}
 	}
-	mWants := pair(relations.muzi)
+	mWants := pair(relations.muzi) // jednostranné páry
 	zWants := pair(relations.zeny)
-	pairs := makePairs(&relations, mWants, zWants)
+	pairs := makePairs(&relations, mWants, zWants) // obostranné
 	for _, p := range pairs {
 		fmt.Printf("%d + %d\n", p[0]+1, p[1]+1)
 	}
